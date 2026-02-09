2d88983fd7b2adb2cadec0f56c4d848e4d71cae4
Roberto Tyley <roberto.tyley@gmail.com>
2026-02-09 13:40:09 +0000

    Better place

--- src/main/scala/com/madgag/logic/time/Timed.scala
+++ src/main/scala/com/madgag/logic/time/Timed.scala
@@ -7,8 +7,10 @@ case class Timed[T: Time, A](interval: BoundedInterval[T], value: A)
 
 type TimedF[T] = [A] =>> Timed[T, A]
 
-def dropTime[T: Time] = new(TimedF[T] ~> cats.Id):
-  def apply[A](t: Timed[T, A]): A = t.value
+object TimedF {
+  def dropTime[T: Time]: TimedF[T] ~> Id = new(TimedF[T] ~> cats.Id):
+    def apply[A](t: Timed[T, A]): A = t.value
 
-given [T: Time]: Functor[TimedF[T]] with
-  def map[A, B](fa: Timed[T, A])(f: A => B): Timed[T, B] = fa.copy(value = f(fa.value))
+  given [T: Time]: Functor[TimedF[T]] with
+    def map[A, B](fa: Timed[T, A])(f: A => B): Timed[T, B] = fa.copy(value = f(fa.value))
+}
